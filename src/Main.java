9484e220c9c7c58e4a950b8a0da1d5c3ee654970
Joey Mussalli <mussallijoey@gmail.com>
2022-10-05 10:12:36 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args){
+        int[] array = new int[]{1, 2, 3, 4, 5};
+        ArrayList<Integer> arraylist = new ArrayList<>();
+        arraylist.add(1);
+        arraylist.add(2);
+        arraylist.add(3);
+        arraylist.add(4);
+        arraylist.add(5);
+    }
+}
